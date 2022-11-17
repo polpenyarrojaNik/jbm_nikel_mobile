0e5a32ac3575a1fb4f23db54759ee721e023c5fa
Pol Peñarroja <ppenarroja@nikel.es>
2022-11-17 08:01:15 +0100

    .

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -15,21 +15,37 @@ import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
+import 'messages_de.dart' as messages_de;
 import 'messages_en.dart' as messages_en;
 import 'messages_es.dart' as messages_es;
+import 'messages_fr.dart' as messages_fr;
+import 'messages_it.dart' as messages_it;
+import 'messages_pt.dart' as messages_pt;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
+  'de': () => new Future.value(null),
   'en': () => new Future.value(null),
   'es': () => new Future.value(null),
+  'fr': () => new Future.value(null),
+  'it': () => new Future.value(null),
+  'pt': () => new Future.value(null),
 };
 
 MessageLookupByLibrary? _findExact(String localeName) {
   switch (localeName) {
+    case 'de':
+      return messages_de.messages;
     case 'en':
       return messages_en.messages;
     case 'es':
       return messages_es.messages;
+    case 'fr':
+      return messages_fr.messages;
+    case 'it':
+      return messages_it.messages;
+    case 'pt':
+      return messages_pt.messages;
     default:
       return null;
   }
