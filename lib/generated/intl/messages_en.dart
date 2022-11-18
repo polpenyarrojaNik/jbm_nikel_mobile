cdfa7a92e592318c12804ebf341cbdcd9bfb7c5c
Pol Peñarroja <ppenarroja@nikel.es>
2022-11-18 11:24:54 +0100

    .

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -81,7 +81,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "articulo_show_articuloDetalle_resumen":
             MessageLookupByLibrary.simpleMessage("Summary"),
         "articulo_show_articuloDetalle_stockYEntregas":
-            MessageLookupByLibrary.simpleMessage("Stock & deliveries"),
+            MessageLookupByLibrary.simpleMessage("Stock & deliveries∫"),
         "articulo_show_articuloDetalle_subfamilia":
             MessageLookupByLibrary.simpleMessage("Subfamily"),
         "articulo_show_articuloDetalle_titulo":
