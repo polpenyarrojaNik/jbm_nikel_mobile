951e83112089f5ea39931e1721c9912ad2ddadc1
Pol Peñarroja <ppenarroja@nikel.es>
2024-09-27 15:05:02 +0200

    articulos ventas orden

--- lib/src/features/articulos/infrastructure/articulo_repository.dart
+++ lib/src/features/articulos/infrastructure/articulo_repository.dart
@@ -224,7 +224,7 @@ class ArticuloRepository {
           OR art.GS1_128_CAJA LIKE '%$searchText%'
           OR art.GS1_128_PALET LIKE '%$searchText%'
           ORDER BY 
-          ${isSearchArticuloForForm ? 'VENTAS_ORDEN,' : ''}
+          ${isSearchArticuloForForm ? 'VENTAS_ORDEN DESC NULLS LAST,' : ''}
           CASE
             WHEN art.ARTICULO_ID = '$searchText' OR art.ARTICULO_ID like'$searchText%' THEN 1
             ELSE 2
