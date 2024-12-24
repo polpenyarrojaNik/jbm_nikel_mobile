efeb7bf09cc7ff5d7d2867ac93594821e9d51cd0
Pol Peñarroja <ppenarroja@nikel.es>
2024-12-24 14:52:12 +0100

    añadir a vistas

--- lib/src/features/visitas/presentation/edit/visit_edit_page.dart
+++ lib/src/features/visitas/presentation/edit/visit_edit_page.dart
@@ -870,7 +870,11 @@ class _SelectClienteWidgetState extends ConsumerState<SelectClienteWidget> {
   void navigateToSearchClientes(BuildContext context) async {
     final customer = await context.router
         .push<Cliente?>(ClienteListaRoute(isSearchClienteForFrom: true));
-    if (customer != null) {
+    if (customer != null && context.mounted) {
+      if (customer.sector == null ||
+          (customer.sector != null && !customer.sector!.isAlta)) {
+        await context.router.push(ClienteSectorRoute(cliente: customer));
+      }
       controller.text = setClienteValue(customer.id, customer.nombreCliente);
       widget.onSelectedCliente(customer.id);
     }
