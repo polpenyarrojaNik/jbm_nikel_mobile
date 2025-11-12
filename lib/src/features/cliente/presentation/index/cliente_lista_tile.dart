74100b30cd6062d5c3a59bbc082477e133f0dfab
Pol Peñarroja <ppenarroja@nikel.es>
2025-11-12 13:21:10 +0100

    añadir icono segun tendencia en la lista de clientes

--- lib/src/features/cliente/presentation/index/cliente_lista_tile.dart
+++ lib/src/features/cliente/presentation/index/cliente_lista_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import '../../../../core/helpers/formatters.dart';
 import '../../../../core/presentation/common_widgets/address_text_widget.dart';
@@ -39,19 +40,59 @@ class ClienteListaTile extends StatelessWidget {
                 ),
             ],
           ),
-          if (!isSameName(cliente))
-            Text(
-              cliente.nombreFiscal,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          AddressTextWidget(
-            codigoPostal: cliente.codigoPostalPredeterminada,
-            poblacion: cliente.poblacionPredeterminada,
-            provincia: cliente.provinciaPredeterminada,
-            pais: cliente.paisPredeterminada,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (!isSameName(cliente))
+                      Text(
+                        cliente.nombreFiscal,
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    AddressTextWidget(
+                      codigoPostal: cliente.codigoPostalPredeterminada,
+                      poblacion: cliente.poblacionPredeterminada,
+                      provincia: cliente.provinciaPredeterminada,
+                      pais: cliente.paisPredeterminada,
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(8),
+              Icon(
+                _getTendenciaIconData(cliente.tendenciaVentas),
+                size: 20,
+                color: _getTendenciaColor(cliente.tendenciaVentas),
+              ),
+            ],
           ),
         ],
       ),
     );
+  }
+
+  IconData _getTendenciaIconData(Tendencia tendencia) {
+    switch (tendencia) {
+      case Tendencia.up:
+        return MdiIcons.arrowUpCircleOutline;
+      case Tendencia.down:
+        return MdiIcons.arrowDownCircleOutline;
+      case Tendencia.equal:
+        return MdiIcons.equal;
+    }
+  }
+
+  Color _getTendenciaColor(Tendencia tendencia) {
+    switch (tendencia) {
+      case Tendencia.up:
+        return Colors.green;
+      case Tendencia.down:
+        return Colors.red;
+      case Tendencia.equal:
+        return Colors.grey;
+    }
   }
 }
